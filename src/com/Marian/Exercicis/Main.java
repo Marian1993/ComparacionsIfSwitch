6b3b903bd4aac758098f111ac98cdb4c65ab3b54
Maria Antonia <marianmartorell.93@gmail.com>
2021-04-14 18:31:37 +0200

    Poc a poc

--- src/com/Marian/Exercicis/Main.java
+++ src/com/Marian/Exercicis/Main.java
@@ -15,7 +15,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(ArraysBidimensional.arrayBidimensionalSimetrica());
+
+        int[][] a1 = {{1, 7, 4}, {5, 12, 10}, {2, 1, 15}, {3, 8, 5}};
+        int[][] a2 = {{1, 9, 3}, {2, 11, 3}, {8, 6, 11}};
+
+        ArraysBidimensional.matriuIdentitat();
     }
 }
 
